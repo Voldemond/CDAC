ece289a4e122b09b99a52f6b6e296092f9a8cd81
Kunal More <136642925+Voldemond@users.noreply.github.com>
2024-09-28 16:09:58 +0530

    Day_3

---
+++ ADSA /Daily Work /Day_3/p1.java
@@ -0,0 +1,34 @@
+package Day_3;
+
+public class p1 {
+	///time complexity calculation 
+	// swapping of two number 
+	public static void main(String a[]) {
+		
+//		int temp= 0 ; 
+//		int aa ; 
+//		int bb ;
+//		
+//		temp = aa ;    // 1 s  
+//		aa = bb ;		   // 1 s 
+//		bb = temp ; 	   // 1 s 
+//					   // f(n) = 3 
+//					
+//		// space complexity calculation 
+//		temp = aa ;    // 1             1 = constant here 
+//		aa = bb ;	   // 1 
+//		bb = temp ;    // 1 
+//					   // s(n) = 3 
+//		//constant time == notation == O (1) 
+//		
+//		// ex. n = 5      8 3 9 7 2 
+////		sum ( A , n )
+////		{
+////			s = 0 ;
+////			for( i =0 ; i < n ; i++ ) {
+////				s= s+ A[i]
+////			}
+////		}
+					
+	}
+}
